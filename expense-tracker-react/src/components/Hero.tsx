8b4badbfbce9e9acd0fe381f25696c66d15820c1
zippy0626 <zippy0626@gmail.com>
2025-08-31 20:09:35 -0400

    feat: added Hero component

---
+++ expense-tracker-react/src/components/Hero.tsx
@@ -0,0 +1,19 @@
+// need to define type for props passed to Hero
+type HeroProps = {
+  onStart: () => void;
+};
+
+export default function Hero({ onStart }: HeroProps) {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen bg-sky-50">
+      <h1 className="text-5xl p-4">Offline Expense Tracker</h1>
+      <button
+        type="button"
+        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow cursor-pointer my-4 outline-0"
+        onClick={onStart}
+      >
+        Get Started
+      </button>
+    </div>
+  );
+}
