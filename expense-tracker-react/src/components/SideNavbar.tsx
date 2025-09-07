6e4dcfa0b8703cfa3b9d218b534e0ea972387002
zippy0626 <zippy0626@gmail.com>
2025-09-06 22:55:44 -0400

    style: add transition to bg to links

--- expense-tracker-react/src/components/SideNavbar.tsx
+++ expense-tracker-react/src/components/SideNavbar.tsx
@@ -6,17 +6,17 @@ export default function SideNavbar() {
     <>
       <section
         id="side-nav-bar"
-        className="z-1 fixed left-0 top-0 h-full w-[200px] overflow-x-hidden bg-gray-200 p-4 transition-[width] duration-200"
+        className="z-1 fixed left-0 top-0 h-full w-[200px] overflow-x-hidden bg-gray-200 p-4"
       >
         <h1 className="p-1 pb-4 font-semibold">Expense Tracker</h1>
         <ul className="flex flex-col gap-2">
-          <li className="cursor-pointer select-none p-1 hover:bg-gray-100 active:bg-gray-300">
+          <li className="cursor-pointer select-none p-1 transition-[background-color] duration-150 hover:bg-gray-100 active:bg-gray-300">
             Dashboard
           </li>
-          <li className="cursor-pointer select-none p-1 hover:bg-gray-100 active:bg-gray-300">
+          <li className="cursor-pointer select-none p-1 transition-[background-color] duration-150 hover:bg-gray-100 active:bg-gray-300">
             Link 2
           </li>
-          <li className="cursor-pointer select-none p-1 transition-[bg] duration-100 hover:bg-gray-100 active:bg-gray-300">
+          <li className="cursor-pointer select-none p-1 transition-[background-color] duration-150 hover:bg-gray-100 active:bg-gray-300">
             Upload Files
           </li>
         </ul>
