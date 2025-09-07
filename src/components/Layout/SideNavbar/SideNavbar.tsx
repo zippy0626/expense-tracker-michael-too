266e347fa1a7e9df24cbaac72709c5dd77272417
zippy0626 <zippy0626@gmail.com>
2025-09-07 12:43:05 -0400

    style: minor changes

--- src/components/Layout/SideNavbar/SideNavbar.tsx
+++ src/components/Layout/SideNavbar/SideNavbar.tsx
@@ -7,17 +7,13 @@ export default function SideNavbar() {
         id="side-nav-bar"
         className="h-full min-w-[200px] overflow-x-hidden bg-gray-200 p-4"
       >
-        <h1 className="pb-4 font-semibold text-xl">Expense Tracker</h1>
+        <h1 className="pb-4 text-xl font-semibold">Expense Tracker</h1>
         <ul className="flex flex-col gap-2">
-          <Link to="/Dashboard">
+          <Link to="/">
             <li className="cursor-pointer p-1 transition-[background-color] duration-150 select-none hover:bg-gray-100 active:bg-gray-300">
               Dashboard
             </li>
           </Link>
-
-          <li className="cursor-pointer p-1 transition-[background-color] duration-150 select-none hover:bg-gray-100 active:bg-gray-300">
-            Link 2
-          </li>
 
           <Link to="/Upload">
             <li className="cursor-pointer p-1 transition-[background-color] duration-150 select-none hover:bg-gray-100 active:bg-gray-300">
