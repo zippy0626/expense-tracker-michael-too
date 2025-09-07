ab66237a17f319219624f1cee374b1e3432195b2
zippy0626 <zippy0626@gmail.com>
2025-09-06 22:39:41 -0400

    refactor: change prop name

--- expense-tracker-react/src/components/Hero.tsx
+++ expense-tracker-react/src/components/Hero.tsx
@@ -1,16 +1,16 @@
 // need to define type for props passed to Hero
 type HeroProps = {
-  onStart: () => void;
+  hide: () => void;
 };
 
-export default function Hero({ onStart }: HeroProps) {
+export default function Hero({ hide }: HeroProps) {
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen bg-sky-50">
       <h1 className="text-5xl p-4">Offline Expense Tracker</h1>
       <button
         type="button"
         className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow cursor-pointer my-4 outline-0"
-        onClick={onStart}
+        onClick={hide}
       >
         Get Started
       </button>
