1208dea3f17e9c37d6b74e006dfd2ce3dfbb9b3a
zippy0626 <zippy0626@gmail.com>
2025-09-08 23:13:43 -0400

    feat: added routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import { useState } from 'react';
-import Hero from './components/Hero.tsx';
-import SideNavbar from './components/SideNavbar.tsx';
-import UploadForm from './components/Upload/UploadForm.tsx';
+import { Outlet } from 'react-router-dom';
+
+import SideNavbar from './components/SideNavbar/SideNavbar.tsx';
 
 function App() {
-  // default state of heroActive = true
-  // when heroActive state changes, React rerenders
-  const [heroActive, showHero] = useState(true);
-
   return (
     <>
-      {heroActive ? (
-        <Hero hide={() => showHero(false)} />
-      ) : (
-        <>
-          <SideNavbar />
-        </>
-      )}
+      <SideNavbar />
+      <Outlet />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Hero from './components/Hero/Hero.tsx';
+import Dashboard from './components/Dashboard/Dashboard.tsx';
+import Upload from './components/Upload/Upload.tsx';
+import App from './App.tsx';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Hero /> },
+  // Dashboard and Upload components injected inside App.tsx thru Outlet
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: 'dashboard', element: <Dashboard /> },
+      { path: 'upload', element: <Upload /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
